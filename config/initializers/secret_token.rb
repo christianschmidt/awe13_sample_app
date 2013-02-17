ca6beb84e00d31724fa46f82cb43951962f7f297
chrische <cs@chrische.net>
2013-02-17 11:20:20 +0100

    Legt Projekt an

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awe13SampleApp::Application.config.secret_token = '38cc1b6deac9b23ef6f69e4b6c7345ff19c69a4cd0b32271e37c2ac6c9ee2a4b987df42b0b880a229747c395e354a5aa1e54a45dbe6be7489c450934f0980894'
